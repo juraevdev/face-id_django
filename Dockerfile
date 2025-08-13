75b554845354686a71aa4c3e689ef4ae69a8d175
davlat <juraevdevpy@gmail.com>
2025-08-13 18:35:35 +0500

    stuff

--- Dockerfile
+++ Dockerfile
@@ -19,7 +19,7 @@ COPY requirements.txt .
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-# Loyiha fayllarini yuklash
+
 COPY . .
 
 # Django uchun environment sozlamalari
--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,6 @@ import environ, os
 
 from datetime import timedelta
 
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
